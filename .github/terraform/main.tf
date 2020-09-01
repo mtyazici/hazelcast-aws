a6033840455b9a6349d959599c07a4eb19a7c162
Muhammet Yazici <muhammet.yazici@boun.edu.tr>
2020-09-01 19:42:31 +0300

    test5

--- .github/terraform/main.tf
+++ .github/terraform/main.tf
@@ -236,6 +236,7 @@ resource "aws_instance" "hazelcast_mancenter" {
     destination = "/home/${var.username}/verify_mancenter.sh"
   }
 
+
   provisioner "file" {
     source      = "hazelcast-client.yaml"
     destination = "/home/${var.username}/hazelcast-client.yaml"
